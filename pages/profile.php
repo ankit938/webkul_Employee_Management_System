b605441ceb1723d6c254198c1e71d4fa0f570f7a
ANKIT MISHRA <ankitmishra1022002@gmail.com>
2025-02-21 22:08:32 +0530

    21/02/2025

--- pages/profile.php
+++ pages/profile.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 
 // Fetch user details
 $user_id = $_SESSION['user_id'];
-$query = "SELECT * FROM users WHERE id='$user_id'";
+$query = "SELECT * FROM users WHERE id=$user_id";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
@@ -27,7 +27,7 @@ if ($result->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
     <title>User Profile</title>
 </head>
 <body>
